e2c099e0bf3749da3e195d8b1580082201059ced
ilcors-dev <corsetti.luca28@gmail.com>
2022-11-19 11:09:16 +0100

    chore(deps): added logger dep

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,18 +3,36 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+use tauri_plugin_log::{LogTarget, LoggerBuilder};
+
 mod audio;
 mod resource;
 mod run;
 
 fn main() {
     tauri::Builder::default()
+        .plugin(
+            LoggerBuilder::default()
+                .targets([
+                    LogTarget::LogDir,
+                    LogTarget::Stdout,
+                    LogTarget::Webview,
+                    LogTarget::Stderr,
+                ])
+                .build(),
+        )
         .invoke_handler(tauri::generate_handler![
             audio::get_audio_devices,
             run::run_voice_recognizer,
             run::stop_voice_recognizer,
             resource::list_path_files
         ])
+        .setup(|app| {
+            #[cfg(debug_assertions)]
+            app.get_window("main").unwrap().open_devtools();
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
