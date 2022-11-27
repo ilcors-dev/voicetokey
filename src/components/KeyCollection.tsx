a58528e8e2fff2e29d7306ea5b0022be1121ad7e
ilcors-dev <corsetti.luca28@gmail.com>
2022-11-27 13:37:42 +0100

    perf: missing key in loop

--- src/components/KeyCollection.tsx
+++ src/components/KeyCollection.tsx
@@ -21,8 +21,8 @@ const KeyCollection = ({ className, keys }: Props) => {
       className={`${className} flex flex-wrap items-center space-x-1`}
     >
       {keys.map((key, index) => (
-        <div className="flex items-center space-x-1">
-          <Key key={`${index}-${key}`} k={key} />
+        <div key={`${index}-${key}`} className="flex items-center space-x-1">
+          <Key k={key} />
           {index !== keys.length - 1 && <span>+</span>}
         </div>
       ))}
